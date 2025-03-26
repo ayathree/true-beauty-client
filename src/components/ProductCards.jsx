310afdb667403c41f9ad427ecd1a6fab004ec75f
ayathree <nobanitaayathree333@gmail.com>
2025-03-26 22:27:20 +0600

    setting tabpanels of react tab

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -1,13 +1,16 @@
 import { BsCart } from "react-icons/bs";
 
 
-const ProductCards = () => {
+const ProductCards = ({product}) => {
+    const {category}=product || {}
+
     return (
-        <div className="border-2 bg-rose-50 shadow-lg mt-7 mx-auto max-w-sm p-6">
+        <div className="border-2 bg-rose-50 shadow-lg  mx-auto max-w-sm p-6">
             <div className="flex justify-center items-center py-6">
             <img className="h-[300px]   border-2 border-rose-600" src="https://d2rd7etdn93tqb.cloudfront.net/wp-content/uploads/2017/07/1320-09C_HBV_HIR_ROSE_HIBISCUS_4OZ-819x1024.jpg" alt="" />
             </div>
             <h1 className="font-bold text-2xl capitalize">card</h1>
+            <p><span className="font-bold">Category :{category}</span> </p>
             <p className="capitalize">Product description and details</p>
             <hr className="mt-2 text-black border border-b-rose-600" />
             <div className="flex  justify-between items-center mt-5 " >
--- src/components/TabCat.jsx
+++ src/components/TabCat.jsx
@@ -3,7 +3,7 @@ import 'react-tabs/style/react-tabs.css'
 import ProductCards from './ProductCards';
 
 
-const TabCat = () => {
+const TabCat = ({products}) => {
  
     return (
       <Tabs>
@@ -20,16 +20,45 @@ const TabCat = () => {
   
       </div>
       <TabPanel>
-        <ProductCards></ProductCards>
+       <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+       {
+          products.filter(p=>p.category === 'Skin Care').map(product=>(
+            <ProductCards key={product._id} product={product}></ProductCards>
+
+          ))
+        }
+       </div>
       </TabPanel>
       <TabPanel>
-        <h2>Any content 2</h2>
+      <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+       {
+          products.filter(p=>p.category === 'Hair Care').map(product=>(
+            <ProductCards key={product._id} product={product}></ProductCards>
+
+          ))
+        }
+        </div>
       </TabPanel>
       <TabPanel>
-        <h2>Any content 2</h2>
+      <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+       {
+          products.filter(p=>p.category === 'Body Care').map(product=>(
+            <ProductCards key={product._id} product={product}></ProductCards>
+
+          ))
+        }
+        </div>
       </TabPanel>
       <TabPanel>
-        <h2>Any content 2</h2>
+      <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+       {
+          products.filter(p=>p.category === 'Makeup Items').map(product=>(
+            <ProductCards key={product._id} product={product}></ProductCards>
+
+          ))
+        }
+        </div>
+        
       </TabPanel>
       </div>
     </Tabs>
