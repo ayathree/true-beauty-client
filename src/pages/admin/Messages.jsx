f0208837dd3358da79803329d8bf43bfe85c3bae
ayathree <nobanitaayathree333@gmail.com>
2025-05-21 16:32:14 +0600

    modify responsive design primary section

--- src/pages/admin/Messages.jsx
+++ src/pages/admin/Messages.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import {  RiDeleteBin7Fill } from "react-icons/ri";
+import Swal from "sweetalert2";
 
 const Messages = () => {
     const { user } = useAuth();
@@ -35,6 +37,38 @@ const Messages = () => {
         window.open(gmailUrl, '_blank');
     };
 
+     const handleDelete = (id) => {
+                Swal.fire({
+                  title: "Are you sure?",
+                  text: "You won't be able to revert this!",
+                  icon: "warning",
+                  showCancelButton: true,
+                  confirmButtonColor: "#3085d6",
+                  cancelButtonColor: "#d33",
+                  confirmButtonText: "Yes, delete it!"
+                }).then(async (result) => {
+                  if (result.isConfirmed) {
+                    try {
+                      await axiosSecure.delete(`/contacts/${id}`);
+                      
+                      await Swal.fire({
+                        title: "Deleted!",
+                        text: "Your product has been deleted.",
+                        icon: "success"
+                      });
+                      
+                      getData();
+                    } catch (err) {
+                      await Swal.fire({
+                        title: "Error!",
+                        text: err.response?.data?.message || "Failed to delete product",
+                        icon: "error"
+                      });
+                    }
+                  }
+                });
+              };
+
     return (
         <div className="mt-10">
             <h2 className="text-rose-600 text-center capitalize text-2xl font-bold mt-10 underline">
@@ -43,6 +77,12 @@ const Messages = () => {
             <div className="m-10 border-2 border-slate-200 px-5 py-2 rounded-xl shadow-xl shadow-rose-200">
                 {contacts.map(contact => (
                     <div key={contact._id} className="mt-5">
+                       <div className="flex justify-end items-end ">
+                         <button onClick={() => handleDelete(contact._id)}  className="btn bg-rose-100 border-2 border-rose-700  text-xl text-rose-700 font-bold">
+                           
+                            <RiDeleteBin7Fill />
+                        </button>
+                       </div>
                         <p className="capitalize">
                             <span className="font-bold text-xl text-rose-700">NAME: </span>
                             {contact.name}
