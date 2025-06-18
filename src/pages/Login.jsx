8ae0a9684f04ccf10cf00bea4f3235daeb5f6411
ayathree <nobanitaayathree333@gmail.com>
2025-06-18 16:50:22 +0600

    add forget password system

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import logo from '../assets/truebeauty_16-removebg-preview.png'
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import toast from "react-hot-toast";
 import axios from "axios";
 import { IoEye, IoEyeOff } from "react-icons/io5";
+import { sendPasswordResetEmail } from "firebase/auth";
+import auth from "../firebase/firebase.config";
 // import useAdmin from "../hooks/useAdmin";
 // import useAxiosSecure from "../hooks/useAxiosSecure";
 
@@ -12,6 +14,7 @@ import { IoEye, IoEyeOff } from "react-icons/io5";
 const Login = () => {
   const navigate = useNavigate()
   const[registerError, setRegisterError]= useState('');
+  const emailRef = useRef(null);
    const location = useLocation()
    const[success, setSuccess] = useState('');
     const[showPass, setShowPass]=useState(false);
@@ -122,6 +125,28 @@ const Login = () => {
     }
 
   }
+
+   const handleForgetPass=()=>{
+  
+        const email = emailRef.current.value;
+        if (!email) {
+          console.log(emailRef.current.value)
+          return;
+  
+          
+        }
+        
+          sendPasswordResetEmail(auth, email)
+          .then(()=>{
+            alert('check your email')
+          })
+          .catch(error=>{
+            console.log(error)
+          })
+  
+      
+  
+      }
   // if ( user || loading) return
   
     return (
@@ -173,7 +198,7 @@ const Login = () => {
          <form onSubmit={handleSignIn}>
          <div className="mt-4">
               <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" htmlFor="LoggingEmailAddress">Email Address</label>
-              <input id="LoggingEmailAddress" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="email" name="email" />
+              <input id="LoggingEmailAddress" ref={emailRef} className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="email" name="email" />
           </div>
   
           <div className="mt-4 relative">
@@ -192,6 +217,7 @@ const Login = () => {
             success && <p className="text-green-600 font-bold">{success}</p>
 
         }
+        <a href="#" onClick={handleForgetPass} className="text-sm hover:text-rose-600 text-gray-600 dark:text-gray-200">Forget Password?</a>
   
           <div className="mt-6">
               <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-rose-600 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
